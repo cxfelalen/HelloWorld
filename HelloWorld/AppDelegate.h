f6402ae09402ecd430f6dd43c0ca4da41c91cbaf
边峥 <massacre@163.com>
2015-08-21 09:57:23 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Bian Zheng on 15/8/21.
+//  Copyright (c) 2015年 Computer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Bian Zheng on 15/8/21.
+//  Copyright (c) 2015年 Computer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
